dd3433d1b55583005370e75e0417b1717fb6c715
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-03-10 07:41:02 +0100

    Pass props to the Foo/Bar components in the example

--- examples/simple.ts
+++ examples/simple.ts
@@ -8,13 +8,13 @@ import {Handle, processStyleProperties, DocumentEmitter} from "../index";
 let emitter = new DocumentEmitter(document);
 let h = new Handle(emitter);
 
-function Foo() {
-    return React.DOM.div({style:{margin: "10px 21px"}}, "Foo");
+function Foo({foo}) {
+    return React.DOM.div({style:{margin: "10px 21px"}}, "Foo", foo);
 }
 
-class Bar extends React.Component<{},{}> {
+class Bar extends React.Component<{bar:string},{}> {
     render() {
-        return React.DOM.div({style:{margin: "10px 22px"}}, "Bar");
+        return React.DOM.div({style:{margin: "10px 22px"}}, "Bar", this.props.bar);
     }
 }
 
@@ -23,8 +23,8 @@ let rootNode = React.DOM.div
     , React.DOM.div({ style: {margin: "10px 21px", "::after": { content: "''", width: "10px", height: "10px", background: "magenta", display: "inline-block",border:"1px solid black", borderRadius: "2px", marginLeft: "40px"}}}, "xxx")
     , React.DOM.span({ style: { color: "red", backgroundColor: "blue" } }, "test 1")
     , React.DOM.span({ style: { color: "red", backgroundColor: "blue" } }, "test 2")
-    , React.createElement(Foo)
-    , React.createElement(Bar)
+    , React.createElement(Foo, { foo: 'from prop' })
+    , React.createElement(Bar, { bar: 'from prop' })
     );
 
 let newRootNode = processStyleProperties(h, React, rootNode);
