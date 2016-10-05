6c8acdecb25174f5e69955ce0b5bcf03ea3e7cf5
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-10-05 22:50:03 +0200

    Decrease GC pressure

--- siphash.ts
+++ siphash.ts
@@ -9,30 +9,25 @@ export const i64Hex = ({h, l}: I64): string =>
     ("0000000" + l.toString(16)).substr(-8);
 
 
+export const _set = (x: I64, h: number, l: number): void => {
+    x.h = h; x.l = l;
+};
+
 const _add = (a: I64, b: I64): void => {
     const rl = a.l + b.l;
-    const h = a.h + b.h + (rl / 2 >>> 31) >>> 0, l =  rl >>> 0;
-
-    a.h = h;
-    a.l = l;
+    _set(a, a.h + b.h + (rl / 2 >>> 31) >>> 0, rl >>> 0);
 };
 
 export const _xor = (a: I64, b: I64): void => {
-    a.h ^= b.h; a.h >>>= 0;
-    a.l ^= b.l; a.l >>>= 0;
+    _set(a, (a.h ^ b.h) >>> 0, (a.l ^ b.l) >>> 0);
 };
 
 const _rotl = (a: I64, n: number): void => {
-    const h = a.h << n | a.l >>> (32 - n), l = a.l << n | a.h >>> (32 - n);
-
-    a.h = h;
-    a.l = l;
+    _set(a, a.h << n | a.l >>> (32 - n), a.l << n | a.h >>> (32 - n));
 };
 
 const _rotl32 = (a: I64): void => {
-    const al = a.l;
-    a.l = a.h;
-    a.h = al;
+    _set(a, a.l, a.h);
 };
 
 const _compress = (v0: I64, v1: I64, v2: I64, v3: I64): void => {
@@ -59,58 +54,77 @@ const _get_int = (a: string, offset: number): number =>
     a.charCodeAt(offset);
 
 
-export const hash = (key: Key, m: string): I64 => {
-    let k0 = { h: key[1] >>> 0, l: key[0] >>> 0 },
-        k1 = { h: key[3] >>> 0, l: key[2] >>> 0 },
-        v0 = { h: k0.h, l: k0.l },
-        v2 = k0,
-        v1 = { h: k1.h, l: k1.l },
-        v3 = k1,
-        mi,
-        mp = 0,
-        ml = m.length,
-        ml7 = ml - 7,
-        buf = new Uint8Array(new ArrayBuffer(8));
 
-    _xor(v0, { h: 0x736f6d65, l: 0x70736575 });
-    _xor(v1, { h: 0x646f7261, l: 0x6e646f6d });
-    _xor(v2, { h: 0x6c796765, l: 0x6e657261 });
-    _xor(v3, { h: 0x74656462, l: 0x79746573 });
-    while (mp < ml7) {
-        mi = { h: _get_int(m, mp + 4), l: _get_int(m, mp) };
+export const hash = (() => {
+    const v0K = {h: 0x736f6d65, l: 0x70736575};
+    const v1K = {h: 0x646f7261, l: 0x6e646f6d};
+    const v2K = {h: 0x6c796765, l: 0x6e657261};
+    const v3K = {h: 0x74656462, l: 0x79746573};
+
+    const i64_0_ff = {h: 0, l: 0xff};
+    const buf = new Uint8Array(new ArrayBuffer(8));
+    const mi = {h: 0, l: 0};
+
+    const k0 = {h: 0, l: 0};
+    const k1 = {h: 0, l: 0};
+
+    const v0 = {h: 0, l: 0};
+    const v1 = {h: 0, l: 0};
+    const v2 = {h: 0, l: 0};
+    const v3 = {h: 0, l: 0};
+
+    return (key: Key, m: string): I64 => {
+        _set(k0, key[1] >>> 0, key[0] >>> 0);
+        _set(k1, key[3] >>> 0, key[2] >>> 0);
+        _set(v0, k0.h, k0.l);
+        _set(v1, k1.h, k1.l);
+        _set(v2, k0.h, k0.l);
+        _set(v3, k1.h, k1.l);
+
+        let mp = 0, ml = m.length, ml7 = ml - 7;
+
+        _xor(v0, v0K);
+        _xor(v1, v1K);
+        _xor(v2, v2K);
+        _xor(v3, v3K);
+        while (mp < ml7) {
+            _set(mi, _get_int(m, mp + 4), _get_int(m, mp));
+            _xor(v3, mi);
+            _compress(v0, v1, v2, v3);
+            _compress(v0, v1, v2, v3);
+            _xor(v0, mi);
+            mp += 8;
+        }
+        buf[7] = ml;
+        let ic = 0;
+        while (mp < ml) {
+            buf[ic++] = m.charCodeAt(mp++);
+        }
+        while (ic < 7) {
+            buf[ic++] = 0;
+        }
+        _set(mi,
+            buf[7] << 24 | buf[6] << 16 | buf[5] << 8 | buf[4],
+            buf[3] << 24 | buf[2] << 16 | buf[1] << 8 | buf[0]);
         _xor(v3, mi);
         _compress(v0, v1, v2, v3);
         _compress(v0, v1, v2, v3);
         _xor(v0, mi);
-        mp += 8;
-    }
-    buf[7] = ml;
-    let ic = 0;
-    while (mp < ml) {
-        buf[ic++] = m.charCodeAt(mp++);
-    }
-    while (ic < 7) {
-        buf[ic++] = 0;
-    }
-    mi = { h: buf[7] << 24 | buf[6] << 16 | buf[5] << 8 | buf[4],
-           l: buf[3] << 24 | buf[2] << 16 | buf[1] << 8 | buf[0] };
-    _xor(v3, mi);
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-    _xor(v0, mi);
-    _xor(v2, { h: 0, l: 0xff });
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
+        _xor(v2, i64_0_ff);
+        _compress(v0, v1, v2, v3);
+        _compress(v0, v1, v2, v3);
+        _compress(v0, v1, v2, v3);
+        _compress(v0, v1, v2, v3);
 
-    let h = v0;
-    _xor(h, v1);
-    _xor(h, v2);
-    _xor(h, v3);
+        const h = {h: 0, l: 0};
+        _set(h, v0.h, v0.l);
+        _xor(h, v1);
+        _xor(h, v2);
+        _xor(h, v3);
 
-    return h;
-};
+        return h;
+    };
+})();
 
 export const string16_to_key = (a: string): Key =>
     [ _get_int(a, 0)
